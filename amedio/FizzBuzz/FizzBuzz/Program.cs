b64661c9ad4813b2dda25b9a003d096e192074d6
NachE <nache.nache@gmail.com>
2011-03-03 04:34:32 +0800

    First README
    Signed-off-by: Amedio <rubenralc@gmail.com>

---
+++ amedio/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter f = new StreamWriter("fizzbuzz.txt");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                f.WriteLine(FizzBuzz.GetFizzBuzz(i.ToString()));
+                f.Flush();
+            }
+
+            f.Close();
+        }
+    }
+}
