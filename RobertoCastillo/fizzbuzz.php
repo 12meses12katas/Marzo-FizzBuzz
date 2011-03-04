524888bc83f910ae1c866d1c3340c75a385faa0b
Roberto Carlos Castillo Medina <colectivo_pulpo@hotmail.com>
2011-03-05 07:58:51 +0800

    Esta es mi primer Kata que intento, espero quede bien jeje

---
+++ RobertoCastillo/fizzbuzz.php
@@ -0,0 +1,12 @@
+<?php
+// Autor:	Roberto Carlos Castillo Medina
+// Date:	03-March-2011
+// License/
+// Usage:	Open Source / for free	
+for($x=1;$x<=100;$x++){
+	if($x%3==0)	echo "FIZZ <br>";
+		else if($x%5==0)echo "BUZZ <br>";
+			else echo $x,"<br>";					
+	}
+
+?>
