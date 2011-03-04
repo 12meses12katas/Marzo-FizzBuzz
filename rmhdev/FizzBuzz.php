b3373a1e1b2f99dcd02f45c00878ec367f6f5b23
Rober Martín H <rmh.dev@gmail.com>
2011-03-05 00:04:29 +0100

    [php] Fixing the recursive solution. Cleaning the code.

--- rmhdev/FizzBuzz.php
+++ rmhdev/FizzBuzz.php
@@ -30,42 +30,45 @@ class FizzBuzz {
     {
         $result = array();
         for ($i=1; $i<= $number; $i++){
-            if (($i % 15) == 0){
-                $result[] = self::FIZZBUZZ;
-            }
-            elseif (($i % 3) == 0){
-                $result[] = self::FIZZ;
-            }
-            elseif (($i % 5) == 0){
-                $result[] = self::BUZZ;
-            }
-            else {
-                $result[] = $i;
-            }
+            $result[] = $this->getCodeForNumber($i);
         }
 
         return implode(" ", $result);
     }
 
+    /**
+     * Write an integer in FizzBuzz style (Recursive)
+     *
+     * @param int $number
+     * @return string
+     */
     function writeRecursive($number){
         
         if (!$number) return "";
 
-        $result = "";
-        if (($i % 15) == 0){
-            $result = self::FIZZBUZZ;
-        }
-        elseif (($i % 3) == 0){
-            $result = self::FIZZ;
-        }
-        elseif (($i % 5) == 0){
-            $result = self::BUZZ;
-        }
-        else {
-            $result = $i;
-        }
+        $result = $this->getCodeForNumber($number--);
 
-        return writeRecursive($number--) . (($result == 1) ? "" : " ") . $result;
+        return $this->writeRecursive($number) . (($result == 1) ? "" : " ") . $result;
+    }
+
+    /**
+     * Get the code for the number
+     *
+     * @param int $number
+     * @return string
+     */
+    protected function getCodeForNumber($number){
+        if (($number % 15) == 0){
+            return self::FIZZBUZZ;
+        }
+        elseif (($number % 3) == 0){
+            return self::FIZZ;
+        }
+        elseif (($number % 5) == 0){
+            return self::BUZZ;
+        }
+        
+        return $number;
     }
 }
 
