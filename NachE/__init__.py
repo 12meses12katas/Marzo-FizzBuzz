0be5332a7d39eb0b38a0c76d8b4723a4e6817e3d
NachE <nache.nache@gmail.com>
2011-03-04 20:00:18 +0100

    al carajo con el usuario
    	deleted:    NachE/__init__.py

--- NachE/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# by J.A. Nache <nache.nache@gmail.com>
-
-for i in range(1,101):
-	if (i%3 == 0 or i%5 == 0) and (i%3 != i%5):
-		print "Fizz" if i%3 == 0 else "Buzz"
-	else:
-		print "FizzBuzz?" if i%3 == 0 and i%5 == 0 else i
