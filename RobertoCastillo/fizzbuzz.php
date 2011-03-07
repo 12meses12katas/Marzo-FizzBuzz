8954145d81ea794852b0d2ad4e57a82a5f65f06d
Roberto Carlos Castillo Medina <colectivo_pulpo@hotmail.com>
2011-03-07 16:24:46 -0600

    Version corregida de FizzBuzz en PHP, gracias a GermanDZ por las correcciones. Saludos

--- RobertoCastillo/fizzbuzz.php
+++ RobertoCastillo/fizzbuzz.php
@@ -2,11 +2,26 @@
 // Autor:	Roberto Carlos Castillo Medina
 // Date:	03-March-2011
 // License/
-// Usage:	Open Source / for free	
-for($x=1;$x<=100;$x++){
-	if($x%3==0)	echo "FIZZ <br>";
-		else if($x%5==0)echo "BUZZ <br>";
-			else echo $x,"<br>";					
-	}
+// Usage:	Open Source / for free
+// Description: Este script muestra la primer parte de la Kata de Marzo FizzBuzz
 
+define("MAXIMO", 100);
+define("MINIMO", 1);
+
+
+for ($num = MINIMO; $num <= MAXIMO; $num++) {
+    if ($num % 3 == 0 && $num % 5 == 0) {
+        echo "FIZZBUZZ <br>";
+    } else {
+        if ($num % 3 == 0) {
+            echo "FIZZ <br>";
+        } else {
+            if ($num % 5 == 0) {
+                echo "BUZZ <br>";
+            } else {
+                echo $num, "<br>";
+            }
+        }
+    }
+}
 ?>
