8f66f30d356ea805bb0ae37d6989b2976b752fe2
eferro <eduardo.ferro.aldama@gmail.com>
2011-03-07 20:39:19 +0800

    Creado isDivisible para evitar repetir codigo (sugerencia GemanDZ)

--- eferro/src/FizzBuzzCounter.java
+++ eferro/src/FizzBuzzCounter.java
@@ -6,28 +6,31 @@ public class FizzBuzzCounter {
 	public FizzBuzzCounter(){
 	}
 	
+	private boolean isDivisible(int number, int divisor) {
+		return number % divisor == 0;
+	}
+	
 	public boolean isFizz(int i){
-		if ((i%3==0) || (String.valueOf(i).indexOf('3')!=-1)) return true;
+		if ((isDivisible(i,3)) || (String.valueOf(i).indexOf('3')!=-1)) return true;
 		return false;
 	}
 	public boolean isBuzz(int i){
-		if ((i%5==0) || (String.valueOf(i).indexOf('5')!=-1)) return true;
+		if (isDivisible(i,5) || (String.valueOf(i).indexOf('5')!=-1)) return true;
 		return false;
 	}
 	
 	public String tick() {
 		String ret = null;
 		counter++;
-		if ((counter % 3 == 0) && (counter % 5 == 0))  {
+		if (isDivisible(counter, 3) && isDivisible(counter,5))  {
 			ret = "FizzBuzz";
-		} else if (counter % 3 == 0)  {
+		} else if (isDivisible(counter, 3))  {
 			ret = "Fizz";
-		} else if(counter % 5 == 0) {
+		} else if(isDivisible(counter, 5)) {
 			ret = "Buzz";
 		} else {
 			ret = String.valueOf(counter); 
 		}
 		return ret;
 	}
-
 }
