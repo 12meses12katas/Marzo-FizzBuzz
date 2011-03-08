abb195144a493bd117f197b41135d6560d67e9c2
Vicente Reig Rincón de Arellano <vicente.reig@justlanded.com>
2011-03-08 20:39:05 +0800

    less shit, more fizz

--- vicentereig/fizzbuzz.go
+++ vicentereig/fizzbuzz.go
@@ -5,5 +5,15 @@ import (
 )
 
 func FizzBuzz() {
-  fmt.Printf("Shiiiiiiiiiiiiiiiiiiit\n")
+  for i:=1; i<101; i++ {
+    if i % 5 == 0 && i % 3 == 0 {
+      fmt.Printf("FizzBuzz!\n")
+    } else if i % 3 == 0 {
+      fmt.Printf("Fizz!\n")
+    } else if i % 5 == 0 {
+      fmt.Printf("Buzz!\n")
+    } else {
+      fmt.Printf("%d!\n", i)
+    }
+  }
 }
