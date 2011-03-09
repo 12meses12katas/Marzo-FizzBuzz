da62c1ff998c7934ca0ce8ee25b2bf5cde7b0114
Alfonso C. García Giménez <agarcia@sirokko.es>
2011-03-10 01:34:13 +0800

    Kata de marzo resuelta [Ruby]

--- karlosgliberal/app.js
+++
@@ -1,9 +0,0 @@
-//id karlosgliberal (patxangas) http://www.investic.net 
-/**
- * FizzBuzz www.12meses12katas.com
- */
-var FizzBuzz = require('./lib/fizzbuzz/fizzbuzz').FizzBuzz;
-
-var app = new FizzBuzz(function(data){
-  console.log(data);
-});
