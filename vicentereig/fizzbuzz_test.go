6a86e6f78f53972702c24adb85d04f4a80a584b4
Vicente Reig Rincón de Arellano <vicente.reig@justlanded.com>
2011-03-09 21:40:31 +0800

    gotest finally PASSES

--- vicentereig/fizzbuzz_test.go
+++ vicentereig/fizzbuzz_test.go
@@ -3,6 +3,7 @@ package gobuzz
 import (
   "testing"
   "strings"
+  "strconv"
 )
 
 func TestFizzBuzz(t *testing.T) {
@@ -14,13 +15,13 @@ func TestFizzBuzz(t *testing.T) {
   for i:=0; i<len(numbers); i++ {
     number = i+1
     if number % 5 == 0 && number % 3 == 0 && !strings.Contains(numbers[i], "FizzBuzz") {
-      t.Errorf("Expecting FizzBuzz at i=%d. Found %s.", i, numbers[i])
+      t.Errorf("Expecting FizzBuzz for %d. Found %s.", number, numbers[i])
     } else if number % 5 == 0 && !strings.Contains(numbers[i], "Buzz") {
-      t.Errorf("Expecting Buzz at i=%d. Found %s.", i, numbers[i])
+      t.Errorf("Expecting Buzz for %d. Found %s.", number, numbers[i])
     } else if number % 3 == 0 && !strings.Contains(numbers[i], "Fizz") {
-      t.Errorf("Expecting Fizz at i=%d. Found %s.", i, numbers[i])
-    } else if !strings.Contains(numbers[i], string(number+49)) {
-      t.Errorf("Expecting %s at i=%d. Found %s.", numbers[i], i, string(number+49))
+      t.Errorf("Expecting Fizz for %d. Found %s.", number, numbers[i])
+    } else if number % 5 != 0 && number % 3 != 0 && !strings.Contains(numbers[i], strconv.Itob(number,10)) {
+      t.Errorf("Expecting %s at %d. Found %s.", strconv.Itob(number, 10), i, numbers[i])
     }
   }
 }
