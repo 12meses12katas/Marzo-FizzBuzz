4439969f7d3ba17f501f98a91486155de3be47ab
Andrés Maneiro <andres.maneiro@gmail.com>
2011-03-13 19:55:52 +0800

    Tests and code for "Buzz" done

--- amaneiro/fizz_buzz.py
+++ amaneiro/fizz_buzz.py
@@ -7,4 +7,8 @@ class FizzBuzz():
         for index in range(len(numbers)):
             if (numbers[index] % 3 == 0):
                 numbers[index] = "Fizz"
+
+            if (numbers[index] != "Fizz" and numbers[index] % 5 == 0):
+                numbers[index] = "Buzz"
+
         return numbers
