95056fba25a970ed351e80b17397b9d7beafd54b
Javier Soto <javiersoto@Javier-Sotos-MacBook-Pro.local>
2011-03-13 19:43:52 +0800

    Mi solución en Objective C para iPhone

---
+++ JaviSoto/FizzBuzzTests/FizzBuzzTests.h
@@ -0,0 +1,19 @@
+//
+//  FizzBuzzTests.h
+//  FizzBuzzTests
+//
+//  Created by Javier Soto on 3/13/11.
+//  Copyright 2011 www.minube.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface FizzBuzzTests : SenTestCase {
+@private
+    
+}
+
+- (void)assertNumber:(NSInteger)number returns:(NSString *)text;
+
+@end
