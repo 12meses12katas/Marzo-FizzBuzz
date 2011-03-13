fd16468f8356896131badd605763e3698b3fd2d8
Andrés Maneiro <andres.maneiro@gmail.com>
2011-03-13 20:00:55 +0800

    Tests and code for numbers which are multiple of 3 and 5

--- amaneiro/fizz_buzz_test.py
+++ amaneiro/fizz_buzz_test.py
@@ -7,21 +7,23 @@ class FizzBuzzTestCase(unittest.TestCase):
 
     def setUp(self):
         self.fb = FizzBuzz()
+        self.numbers = self.fb.execute()
 
     def testCountUntil100(self):
-        self.assertEqual(100, len(self.fb.execute()))
+        self.assertEqual(100, len(self.numbers))
 
     def testRaisesFizzWhenNumberIsMultipleOf3(self):
-        numbers = self.fb.execute()
-        self.assertEqual('Fizz', numbers[2])
-        self.assertEqual('Fizz', numbers[29])
-        self.assertEqual('Fizz', numbers[98])
+        self.assertEqual('Fizz', self.numbers[2])
+        self.assertEqual('Fizz', self.numbers[98])
 
     def testRaisesBuzzWhenNumberIsMultipleOf5(self):
-        numbers = self.fb.execute()
-        self.assertEqual('Buzz', numbers[4])
-        self.assertEqual('Buzz', numbers[49])
-        self.assertEqual('Buzz', numbers[94])
+        self.assertEqual('Buzz', self.numbers[4])
+        self.assertEqual('Buzz', self.numbers[49])
+        self.assertEqual('Buzz', self.numbers[94])
+
+    def testNumbersAreMultipleOf3And5(self):
+        self.assertEqual('FizzBuzz', self.numbers[14])
+        self.assertEqual('FizzBuzz', self.numbers[29])
 
 if __name__ == '__main__':
     unittest.main()
