2edd481f43bb5658af90ffbf745adecfcc84fa66
Javier Lopez <jlopez@fi.upm.es>
2011-03-17 00:18:06 +0800

    jlopez: Kata completa

--- jlopez/fizzbuzz.py
+++ jlopez/fizzbuzz.py
@@ -4,18 +4,19 @@ import sys
 
 def fizzbuzz(maximum):
     for number in range(1,maximum):
-        if number % 3 != 0 and number % 5 != 0:
-            print(number)
-        else:
-            if number % 3 == 0:
-                sys.stdout.write("Fizz")
-            if number % 5 == 0:
-                sys.stdout.write("Buzz")
-            sys.stdout.write("\n")
+        result = ""
+        if number % 3 == 0 or str(number).find("3") != -1:
+            result = "Fizz"
+        if number % 5 == 0 or str(number).find("5") != -1:
+            result += "Buzz"
+            if len(result) > 4:
+                result +="?"
+        if len(result) == 0:
+            result = number
+        print result
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         fizzbuzz(100)
     else:
         fizzbuzz(int(sys.argv[1]))
-        
