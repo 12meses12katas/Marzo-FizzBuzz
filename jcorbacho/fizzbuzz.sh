3a82d63536cbb263af4eccf8fe1be66643c56151
Jose Corbacho Gil <jose@corbacho.net>
2011-03-25 16:54:28 +0100

    Test: first upload

---
+++ jcorbacho/fizzbuzz.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+## -----------------------------------------------------------
+## Author: Jose Corbacho Gil (jose@corbacho.net)
+## Date: 2011/03/05
+## Version: 1.0
+## Marzo-FizzBuzz
+## -----------------------------------------------------------
+
+## isDivisible
+isDivisible ()
+{
+   result=0
+   number=$1
+   modul=$2
+   let MODULUS=$number%$modul
+   [ $MODULUS -eq 0 ] && result=1  
+   return $result
+
+}
+
+## hasNumber
+hasNumber()
+{
+   result=0
+   number=$1
+   numberToCheck=$2
+   echo $number | grep $numberToCheck > /dev/null 2>&1
+   [ $? -eq 0 ] && result=1
+   return $result
+
+}
+
+
+
+## Main code
+## -----------------------------------------------
+
+for i in `seq 1 100`;do
+    isDivisible $i 3
+    fizz=$?
+
+    if [ $fizz -eq 0 ]; then
+       hasNumber $i 3
+       fizz=$?
+    fi
+
+    isDivisible $i 5
+    buzz=$?
+
+    if [ $buzz -eq 0 ]; then
+       hasNumber $i 5
+       buzz=$?
+    fi
+
+
+    if [[ $fizz -eq 1 && $buzz -eq 1 ]]; then
+       echo "FizzBuzz"
+    elif [ $fizz -eq 1 ]; then
+       echo "Fizz"
+    elif [ $buzz -eq 1 ]; then
+       echo "Buzz"
+    else 
+       echo $i
+    fi
+done
+
