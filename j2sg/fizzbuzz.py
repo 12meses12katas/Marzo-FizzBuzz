8a9f721b540e8b65eac623a254997f069a52b7c1
Juan José Salazar García <jjslzgc@gmail.com>
2011-03-31 18:17:08 +0200

    FizzBuzz en marcha

--- j2sg/fizzbuzz.py
+++ j2sg/fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-# 12 Meses 12 Katas : Marzo FizzBuzz
-# By j2sg : jjslzgc@gmail.com
+# 12 Meses 12 Katas - Marzo FizzBuzz
+# By j2sg - jjslzgc@gmail.com
 
 for n in range(1,101):
 	print '%s%s%s'%((['','Fizz'][n%3==0]),(['','Buzz'][n%5==0]),(['',str(n)][n%3!=0 and n%5!=0]))
