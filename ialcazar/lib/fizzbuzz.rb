334dec90dd052c512de6c71fac3f6209052a9546
ialcazar <israelalcazar@gmail.com>
2011-04-01 04:44:43 +0800

    Parte segunda completada en Ruby

--- ialcazar/lib/fizzbuzz.rb
+++ ialcazar/lib/fizzbuzz.rb
@@ -9,12 +9,20 @@ class Fixnum
     self % 5 == 0
   end  
   
+  def contiene_un_tres?
+    self.to_s.include? '3'
+  end
+  
+  def contiene_un_cinco?
+    self.to_s.include? '5'
+  end
+  
   public
   
   def fizzbuzz
     return "FizzBuzz" if divisible_por_tres? and divisible_por_cinco?
-    return "Fizz" if divisible_por_tres?
-    return "Buzz" if divisible_por_cinco?
+    return "Fizz" if divisible_por_tres? or contiene_un_tres?
+    return "Buzz" if divisible_por_cinco? or contiene_un_cinco?
     self
   end
   
