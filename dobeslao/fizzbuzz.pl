75f2cdf92052cf6919ef4ce37be16b8d927e1dd8
StarChox <starchox@gmail.com>
2011-04-02 02:38:43 +0800

    Factorización del código perl, agregando set de pruebas.

--- dobeslao/fizzbuzz.pl
+++ dobeslao/fizzbuzz.pl
@@ -1,19 +1,9 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 
-#use strict;
+use strict;
 
-for ($i = 1; $i <= 100; $i++) {
-#	print " hola  $i\n";
-	$fizzbuzz = '';
+require 'fizzbuzz_func.pl';
 
-	if ( $i%3 == 0 || $i =~ m/3/ ) {
-		$fizzbuzz = 'Fizz';
-	}
-
-	if ( $i%5 == 0 || $i =~ m/5/ ) {
-		$fizzbuzz .= 'Buzz';
-	}
-	
-	print length($fizzbuzz) == 0 ? "$i" : "$fizzbuzz";
-	print "\n";
+for (my $i = 1; $i <= 100; $i++) {
+	print fizzbuzz($i) . "\n";
 }
