cfbfbc990406393ca897ab0e7d966d74d02c37a5
Jesus Alberto Paz Jimenez <alberto.pazjimenez@gmail.com>
2011-04-11 23:16:48 +0200

    Implemented stage2

--- japaz/src/main/java/com/japaz/katas/mfb/NumberGenerator.java
+++ japaz/src/main/java/com/japaz/katas/mfb/NumberGenerator.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NumberGenerator {
-	List<NumberPrinter> numberPrinters = new ArrayList<NumberPrinter>();
+	List<NumberType> numberPrinters = new ArrayList<NumberType>();
 	
-	public void addNumberPrinter(NumberPrinter numberPrinter) {
+	public void addNumberPrinter(NumberType numberPrinter) {
 		numberPrinters.add(numberPrinter);
 	}
 	
 	public String printNumber(int number) {
 		StringBuffer numberRepresentation = new StringBuffer();
 		
-		for (NumberPrinter numberPrinter : numberPrinters) {
+		for (NumberType numberPrinter : numberPrinters) {
 			numberRepresentation.append(numberPrinter.print(number));
 		}
 		
--- japaz/src/test/java/com/japaz/katas/mfb/NumberPrinter.java
+++
@@ -1,5 +0,0 @@
-package com.japaz.katas.mfb;
-
-public interface NumberPrinter {
-	public String print(int number);
-}
