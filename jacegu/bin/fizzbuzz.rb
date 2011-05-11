75343597790cefe848c4d1dd4693cca965671659
Javier Acero <j4cegu@gmail.com>
2011-05-11 22:44:59 +0200

    print to a 100 as expected

--- jacegu/bin/fizzbuzz.rb
+++ jacegu/bin/fizzbuzz.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 
 $: << File.join(File.expand_path(File.dirname(__FILE__)), "..")
+
 require 'lib/fizzbuzz.rb'
 
-up_limit = ARGV[0].to_i
-numbers = (1..up_limit).to_a
-list = FizzBuzz::NumberList.new(numbers)
+list = FizzBuzz::NumberList.new(1..100)
 list.print_to(STDOUT)
